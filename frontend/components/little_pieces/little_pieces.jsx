b1dcb63cdace00140520ad8aa9011dcca692f837
ishafran <isabelle.shafran@gmail.com>
2022-02-01 21:03:25 -0500

    remove clickouts in flowers section

--- frontend/components/little_pieces/little_pieces.jsx
+++ frontend/components/little_pieces/little_pieces.jsx
@@ -30,36 +30,37 @@ class LittlePieces extends React.Component {
             <div className="friendship_container">
                 <div className="project-index-header">Little Pieces of Death</div>
                     <div className="project-index-description">
+                        <p>
                         This is a series of photos and installations made from plaster and latex casts of flowers. 
                         I make silicone molds from cosmo flowers cut from the garden and use the molds to make casts. 
-                        I began this body of work while staying at a farm in the English countryside during the first Covid 
-                        lockdown. Overwhelmed by the floral beauty I encountered I obsessively photographed the garden 
-                        flowers daily. There was a manic quality to my desire to record each bit of beauty I came across, 
-                        knowing it would be gone soon. This activity connected to my love of still life, and how historically,
-                        paintings of flower bouquets serve as symbols of the fleetingness of life. A cut flower, while lovely to 
-                        look at, has a short time to bloom before it inevitably wilts and dies. 
-                        
-                        My interest in collecting and preserving short-lived beauty led me to explore the indexical 
-                        relationship between original and reproduction, and the process of transformation. Wanting to
-                         find ways to preserve beyond photographing, I use the flowers to make molds. This process is 
-                         necessarily destructive; the casts made from these molds become a death shroud, their existence 
-                         is proof of the flower’s demise. Fragments of petals are left in it's hard, plaster copy; residue 
-                         and traces of past life. My desire to flout the ravages of time on delicate existence, creates lasting 
-                         little pieces of death; a flower that cannot wilt, yet has nothing alive within it. 
-
-                        With each copy made, the original flower further decays and the plaster casts become empty and bonelike.
-                         Dripping color into these porous objects, transforms them into a record of the slow movement of the bright 
-                         liquid through its body. Photographing and printing these variations, changes their scale and dimension. 
-                         This gives them new life and a new kind of beauty, obscured from the distant original into something fresh,
-                          but not entirely new. 
-
-                        The latex versions, void of living matter, recall a true flower in their reactions to light and air. 
-                        Pinned to the wall as a collection of specimens, they are uncanny imitations of the original, with the 
-                        irie appearance of human skin which cannot decay. Lively, yet not alive. 
+                        I began this body of work while staying at a farm in the English countryside during the first Covid lockdown. 
+                        Overwhelmed by the floral beauty I encountered I obsessively photographed the garden flowers daily. 
+                        There was a manic quality to my desire to record each bit of beauty I came across, knowing it would be gone soon. 
+                        This activity connected to my love of still life, and how historically, paintings of flower bouquets serve as 
+                        symbols of the fleetingness of life. A cut flower, while lovely to look at, has a short time to bloom before it 
+                        inevitably wilts and dies.  
+                        </p>
+                        <p>
+                        My interest in collecting and preserving short-lived beauty led me to explore the indexical relationship 
+                        between original and reproduction, and the process of transformation. Wanting to find ways to preserve beyond 
+                        photographing, I use the flowers to make molds. This process is necessarily destructive; the casts made from these 
+                        molds become a death shroud, their existence is proof of the flower’s demise. My desire to flout the ravages of 
+                        time on delicate existence, creates lasting little pieces of death; a flower that cannot wilt, but contains no life.
+                        </p>
+                        <p>
+                        I drip color into these bonelike, porous objects, transforming them into a record of the slow movement of the 
+                        bright liquid through its body. Photographing and printing these variations, changes their scale and dimension. 
+                        This gives them new life and a new kind of beauty, obscured from the distant original into something fresh, 
+                        but not entirely new.
+                        </p>
+                        <p>
+                            Below is a selection of pieces from the series, to view the full collection please see my catalogue.
+                        </p>
                     </div>
                 <div className="little-pieces-render">
                     {paintingRender()}
                 </div>
+                <div className="flower-catalogue">Click <a className="pdf" href={window.flower_pdf} download="AvailableFlowerArt"> here </a>to download full collection</div>
             </div>
             );
     }
